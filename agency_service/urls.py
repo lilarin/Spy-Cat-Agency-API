df699ee45f2e7c0131f75c89ee622122e0a0ba1e
lilarin <80650911+lilarin@users.noreply.github.com>
2024-11-09 16:20:04 +0200

    Minor improvements and import refinements

--- agency_service/urls.py
+++ agency_service/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
